fd86685f66ca3e8421002407c9dcf168b13abc3e
Rares <rares.andrei.tabacaru@gmail.com>
2023-04-25 12:12:52 +0300

    Update: Am adaugat clasele Role si User + migratie in baza de date

--- DataLayer/AppDbContext.cs
+++ DataLayer/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace DataLayer
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                    .UseSqlServer("Server=localhost;Database=LabProject;User Id=adonici;Password=123456;")
+                    .UseSqlServer("Server=localhost;Database=LabProject;User Id=Rares2;Password=1234;")
                     .LogTo(Console.WriteLine);
         }
 
@@ -22,5 +22,7 @@ namespace DataLayer
         public DbSet<Class> Classes { get; set; }
         public DbSet<Grade> Grades { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
     }
 }
